649b33c9204f19c4d9cc9617c3ad43eaec8bbdb7
Adam Ochotnicki <adamochotnicki@gmail.com>
2020-04-14 18:44:33 +0100

    firebase conf, new-training - listening to value changes of firestore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAOQJEwQbzur_oghx0Lz9KSI5IA9wB4ps4",
+    authDomain: "ng-fitness-tracker-46f67.firebaseapp.com",
+    databaseURL: "https://ng-fitness-tracker-46f67.firebaseio.com",
+    projectId: "ng-fitness-tracker-46f67",
+    storageBucket: "ng-fitness-tracker-46f67.appspot.com",
+    messagingSenderId: "393683819302",
+    appId: "1:393683819302:web:6262158cc97c8ef120bfe7",
+    measurementId: "G-FWTER7WLH4"
+  }
 };
 
 /*
