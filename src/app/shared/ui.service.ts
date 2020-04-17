268c5c0b948b69d3640a998a313589906d4793ec
Adam Ochotnicki <adamochotnicki@gmail.com>
2020-04-17 16:33:19 +0100

    reusable snackbar - method in ui service

--- src/app/shared/ui.service.ts
+++ src/app/shared/ui.service.ts
@@ -1,6 +1,6 @@
+import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Subject } from 'rxjs';
-import { Injectable } from '@angular/core';
 
 @Injectable()
 export class UIService {
