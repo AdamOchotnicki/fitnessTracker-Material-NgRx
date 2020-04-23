b9243d7af0d1e352e81620d5eb8d6d9df355dc3d
Adam Ochotnicki <adamochotnicki@gmail.com>
2020-04-24 00:22:53 +0100

    dispatching actions & selecting state slices

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -7,7 +7,7 @@ import { Store } from '@ngrx/store';
 
 import { AuthService } from '../auth.service';
 import { UIService } from '../../shared/ui.service';
-import * as fromApp from '../../app.reducer';
+import * as fromRoot from '../../app.reducer';
 
 @Component({
   selector: 'app-login',
@@ -23,11 +23,11 @@ export class LoginComponent implements OnInit {
     private authService: AuthService,
     //private afAuth: AngularFireAuth,
     //private uiService: UIService,
-    private store: Store<{ui: fromApp.State}>
+    private store: Store<fromRoot.State>
   ) { }
 
   ngOnInit() {
-    this.isLoading$ = this.store.pipe(map(state => state.ui.isLoading));
+    this.isLoading$ = this.store.select(fromRoot.getIsLoading);
     // this.loadingSubs = this.uiService.loadingStateChange.subscribe(isLoading => {
     //   this.isLoading = isLoading;
     // });
