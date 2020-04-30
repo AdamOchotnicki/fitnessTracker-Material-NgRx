9a4dd91fc2d592855343505289f31ad1238e9805
Adam Ochotnicki <adamochotnicki@gmail.com>
2020-04-30 12:16:31 +0100

    environment for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAOQJEwQbzur_oghx0Lz9KSI5IA9wB4ps4",
+    authDomain: "ng-fitness-tracker-46f67.firebaseapp.com",
+    databaseURL: "https://ng-fitness-tracker-46f67.firebaseio.com",
+    projectId: "ng-fitness-tracker-46f67",
+    storageBucket: "ng-fitness-tracker-46f67.appspot.com",
+    messagingSenderId: "393683819302",
+    appId: "1:393683819302:web:6262158cc97c8ef120bfe7",
+    measurementId: "G-FWTER7WLH4"
+  }
 };
